9d743ced402daf8fc4780a21189c934b5b96993e
Evan <suzukaze.hazuki2020@gmail.com>
2022-04-29 17:25:09 +0900

    remove backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "amplifydemo": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
